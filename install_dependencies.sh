15790dd927034a50fabe29ab1edd17ec15193b6a
timothy kibalama <timkibalama@gmail.com>
2023-05-24 13:27:36 +0300

    added dependencies installer

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -11,9 +11,9 @@ php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan view:cache
-php artisan storage:link
 MIGRATE_FILE=migrate
 if test -f "$MIGRATE_FILE"; then
     php artisan migrate
     rm "$MIGRATE_FILE"
+    php artisan storage:link
 fi
