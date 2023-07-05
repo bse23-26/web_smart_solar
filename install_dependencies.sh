e3016ba88e1ad38469b968aeab9555072924781f
Timothy Kibalama <timkibalama@gmail.com>
2023-07-05 08:26:38 +0300

    finished

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,14 +9,14 @@ php composer.phar install --optimize-autoloader --no-dev
 npm install
 npm run build
 rm -r node_modules/
-#MIGRATE_FILE=migrate
-#if test -f "$MIGRATE_FILE"; then
-#    php artisan key:generate
-#    php artisan migrate:refresh
-#    php artisan db:seed
-#    rm "$MIGRATE_FILE"
-#    php artisan storage:link
-#fi
+MIGRATE_FILE=migrate
+if test -f "$MIGRATE_FILE"; then
+    php artisan key:generate
+    php artisan migrate:refresh
+    php artisan db:seed
+    rm "$MIGRATE_FILE"
+    php artisan storage:link
+fi
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
